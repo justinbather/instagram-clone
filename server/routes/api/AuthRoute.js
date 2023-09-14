dda8159616c2717488821a295e4cf20d5e11a20f
justinbather <justinbather@hotmail.com>
2023-09-14 19:45:22 -0400

    added home page auth

--- server/routes/api/AuthRoute.js
+++ server/routes/api/AuthRoute.js
@@ -1,10 +1,12 @@
 import {Signup, Login} from "../../controllers/AuthController.js";
+import { userVerification } from "../../middlewares/AuthMiddleware.js";
 
 import express from "express";
 
 const router = express.Router();
 
 router.post("/signup", Signup);
-router.post("/login", Login)
+router.post("/login", Login);
+router.post('/home', userVerification);
 
 export default router;
